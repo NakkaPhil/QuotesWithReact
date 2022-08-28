edd093e4598f21d065e9aa04645a997b1879095d
Philippe <sevasdoge.01@gmail.com>
2022-08-28 19:41:26 -0400

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import QuoteBox from './components/QuotesBox'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <QuoteBox/>
+    </div>
+  ) 
+}
+
+export default App
---
+++ src/components/QuotesBox.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import quotesList from './quotes.json'
+
+const QuoteBox = () => {
+    
+    let colors = ['120B02','2E3830', '916827', 'F0D02D', '251704', '44CFCB', '4EA5D9', '2A4494', '224870', '122C34'];
+    
+    //Generating random indexes inside our colors array:
+    const randomColorIndex = () => { return Math.floor( Math.random() * colors.length ) }
+
+    //Setting 'colorIndex' as a random index in first place, before rendering
+    const [colorIndex, setColorIndex] = useState(randomColorIndex)
+    
+    //Apliyng that colour to the body
+    document.body.style.backgroundColor = '#' + colors[colorIndex]
+
+
+    //Function to change the background, button and text colours:
+    const changeColor = ()=> { 
+        setColorIndex(randomColorIndex)
+    }
+    
+    //Generating random indexes for the quotes array (quotes.json)
+    const randomIndex = () => {  return Math.floor( Math.random() * quotesList.length ) } 
+    const [index, setIndex] = useState(randomIndex)
+
+    const changeQuote = () => {  setIndex(randomIndex) }
+
+    //This will execute the button downside
+    function changeValues() {
+        changeColor();
+        changeQuote();
+    }
+    
+    return (
+        <div className="quoteBox" style={{color: `#${colors[colorIndex]}`}}>
+            
+            <div className='quote_Desc'>
+                <img src="./src/assets/left-quote.png" id='quote_icon' />
+                <p>{quotesList[index].quote}</p>
+            </div>
+
+            <div className='quote_Author'>
+                <h3><em>{quotesList[index].author}</em></h3>
+                
+                <button onClick={changeValues} style={{backgroundColor: `#${colors[colorIndex]}`}}>
+                    <img src="./src/assets/right-arrow.png" alt="" />
+                </button>
+            
+            </div>
+        </div>
+    )
+}
+
+export default QuoteBox
